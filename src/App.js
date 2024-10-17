810a44380e119858997e7aea294b7474c41d9219
Nouaamane <norou6@gmail.com>
2024-10-17 17:24:19 +0100

    feat create component with static data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import './App.css';
 
-function App() {
+function Header(){
+  return <header>
+    <h1>
+    Fast React  Pizza ! 
+  </h1>
+  </header>
+}
+
+
+
+function Menu(){
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello world, how is the Weather
-      </header>
+    <main className='menu'>
+          <h2>
+            Our Menu.
+          </h2>
+            <Pizza/>
+            <Pizza/>
+            <Pizza/>
+            <Pizza/>
+    </main>
+  )
+}
+
+function Pizza(){
+  return (
+    <div>
+      <img src='./pizzas/focaccia.jpg' alt='focaccia pizza'/>
+      <h3>Pizza Spinaci</h3>
+      <p>Tomato, mozarela, spinach, and ricotta cheese</p>
+    </div>
+  )
+}
+
+function Footer(){
+  return <footer className='footer'>
+   {new Date().toLocaleString()}, We're currently open.
+  </footer >
+}
+
+function App() {
+
+  return (
+    <div className="container">
+        <Header/>
+        <Menu/>
+        <Footer/>
     </div>
   );
 }
