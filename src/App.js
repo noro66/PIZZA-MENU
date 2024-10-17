82ed89651daeca3bd9dc42fb95f285540668c84f
Nouaamane <norou6@gmail.com>
2024-10-17 22:29:07 +0100

    feat rendring piza data using map function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,48 @@
 import './App.css';
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
 function Header(){
   return <header>
@@ -16,21 +60,25 @@ function Menu(){
           <h2>
             Our Menu.
           </h2>
-            <Pizza/>
-            <Pizza/>
-            <Pizza/>
-            <Pizza/>
+       <ul className='pizzas'>
+         {  pizzaData.map((pizza)=>{
+          return  <Pizza pizza={pizza}/>
+          })}
+       </ul>
     </main>
   )
 }
 
-function Pizza(){
+function Pizza({pizza}){
   return (
-    <div>
-      <img src='./pizzas/focaccia.jpg' alt='focaccia pizza'/>
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarela, spinach, and ricotta cheese</p>
-    </div>
+    <li className='pizza'>
+      <img src={pizza.photoName} alt={pizza.photoName}/>
+     <div>
+       <h3>{pizza.name}</h3>
+      <p>{pizza.ingredients}</p>
+      <span>{pizza.price}</span>
+     </div>
+    </li>
   )
 }
 
