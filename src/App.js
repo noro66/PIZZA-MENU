782259a7a98a7c05e887b28cbaf2ceb3328d3ec8
Nouaamane <norou6@gmail.com>
2024-10-17 16:16:42 +0100

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello world, how is the Weather
+      </header>
+    </div>
+  );
+}
+
+export default App;
