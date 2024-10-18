cbad0a1439b44b2201be7cb1df3684faf125e90d
Nouaamane <norou6@gmail.com>
2024-10-18 11:33:36 +0100

    feat change style using terniry opiration

--- src/App.js
+++ src/App.js
@@ -70,14 +70,14 @@ function Menu(){
 }
 
 function Pizza({pizza}){
-  if(pizza.soldOut) return null;
+  // if(pizza.soldOut) return null;
   return (
-    <li className='pizza'>
+    <li className={`pizza ${pizza.soldOut ? 'sold-out' : ''}`} >
       <img src={pizza.photoName} alt={pizza.photoName}/>
      <div>
        <h3>{pizza.name}</h3>
       <p>{pizza.ingredients}</p>
-      <span>{pizza.price}</span>
+      <span>{pizza.soldOut ? "SOLD OUT!" : pizza.price}</span>
      </div>
     </li>
   )
